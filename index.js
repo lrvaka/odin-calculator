40157e7dee013357aa9d994eba1eb6402bb5e20b
lrvaka <lukevakasisikakala@gmail.com>
2022-07-14 17:58:19 -0400

    add decimal func

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ const numberHandler = (value) => {
 
 const operatorHandler = (value) => {
   if (value === "CE") {
-    console.log("hello")
     screenValue = ""
     firstValue = 0
     secondValue = ""
@@ -48,37 +47,49 @@ const operatorHandler = (value) => {
     screen.textContent = "0"
   } else if (value === "=") {
     if (operator) {
-      //equals is not an operator silly
       firstValue = operate(operator, +firstValue, +secondValue)
       screenValue = firstValue // screen now equals firstValue
-      screen.textContent = screenValue
       operator = "" // reset operator to nothing
       secondValue = "" // second value to nothing
+    } else if (secondValue) {
+      screenValue = secondValue
+      firstValue = secondValue
+      operator = "" // set new operator
+      secondValue = ""
     } else {
       //if there is no operator
-      screenValue = firstValue
-      screen.textContent = screenValue
+      firstValue = screenValue
       operator = "" // set new operator
       secondValue = ""
     }
   } else if (value === ".") {
+    if (!screenValue.includes(".")) {
+      screenValue += "."
+      if (secondValue) {
+        secondValue += "."
+      }
+    }
   } else if (secondValue) {
-    //previously set operator, not current one
-    firstValue = operate(operator, +firstValue, +secondValue)
-    screenValue = firstValue
-    screen.textContent = screenValue
-    operator = value // set new operator
-    secondValue = "" //set second value to zero
+    if (operator) {
+      //previously set operator, not current one
+      firstValue = operate(operator, +firstValue, +secondValue)
+      screenValue = firstValue
+      operator = value // set new operator
+      secondValue = "" //set second value to zero
+    } else {
+      firstValue = screenValue
+      secondValue = ""
+      operator = value
+    }
   } else {
     firstValue = screenValue //Set first value to the value onscreen
     operator = value //Set the operator to value
   }
+  screen.textContent = screenValue
   console.log({ firstValue })
   console.log({ secondValue })
   console.log({ operator })
 }
-
-//(!firstValue)
 
 buttons.addEventListener("click", (e) => {
   let target = e.target.closest(".btn")
